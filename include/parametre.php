cbbc50d9d404b1d1577261a1a577236d05ec280e
flodday <e.mpc41@gmail.com>
2024-11-26 15:19:11 +0100

    first commit

---
+++ include/parametre.php
@@ -0,0 +1,8 @@
+<?php
+// Pour connexion à BD
+define('NOM', 'root');
+define('PASSE', '');
+define('SERVEUR', 'localhost');
+define('BASE', 'hotel');
+
+ini_set('display_errors', 1);
---
+++ templates_c/5329f7c0850fbe036b37d2817df7d074800e0b6e_0.file.header.tpl.php
@@ -0,0 +1,65 @@
+<?php
+/* Smarty version 4.3.4, created on 2024-11-26 14:53:38
+  from 'C:\xampp\htdocs\hotels\public\header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.4',
+  'unifunc' => 'content_6745d2e2dcf3a8_83655623',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5329f7c0850fbe036b37d2817df7d074800e0b6e' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\hotels\\public\\header.tpl',
+      1 => 1732625941,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6745d2e2dcf3a8_83655623 (Smarty_Internal_Template $_smarty_tpl) {
+?>       <!-- Header-->
+        <header id="header" class="header">
+
+            <div class="header-menu">
+
+                <div class="col-sm-7">
+                    <a id="menuToggle" class="menutoggle pull-left"><i class="fa fa fa-tasks"></i></a>
+                    <div class="header-left">
+                        <button class="search-trigger"><i class="fa fa-search"></i></button>
+                        <div class="form-inline">
+                            <form class="search-form">
+                                <input class="form-control mr-sm-2" type="text" placeholder="Search ..." aria-label="Search">
+                                <button class="search-close" type="submit"><i class="fa fa-close"></i></button>
+                            </form>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="col-sm-5">
+                    <div class="user-area dropdown float-right">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="user-avatar rounded-circle" src="public/images/admin.jpg" alt="User Avatar">
+                        </a>
+
+                        <div class="user-menu dropdown-menu">
+                                <a class="nav-link" href="#A VOUS D'ECRIRE LE LIEN"><i class="fa fa-power -off"></i>Déconnexion</a>
+                        </div>
+                    </div>
+                        <div class="user-area">
+                        Bienvenue <?php echo $_smarty_tpl->tpl_vars['login']->value;?>
+
+                        </div>
+
+                </div>
+            </div>
+
+        </header><!-- /header -->
+        <!-- Header-->
+<?php }
+}
